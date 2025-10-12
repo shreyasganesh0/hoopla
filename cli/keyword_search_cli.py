310b4631156b0fab25460f2bafb5163fd4ff22bf
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-10-12 06:41:23 -0400

    feat: add string sanitization to lower case and punctuation removal

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+import string
 
 
 def main() -> None:
+
+
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
@@ -15,6 +19,24 @@ def main() -> None:
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
+            fp = open("data/movies.json", "r")
+            data = json.load(fp)
+            movies_list = []
+
+            t_table = str.maketrans(dict.fromkeys(string.punctuation, None))
+
+            for movie in data["movies"]:
+
+                if args.query.lower().translate(t_table) in movie["title"].lower().translate(t_table):
+
+                    movies_list.append(movie)
+            movies_list.sort(key = lambda a : a["id"])
+
+            i = 1
+            for mv in movies_list:
+                print(f"{i}. {mv["title"]}") 
+                i +=1
+
         case _:
             parser.print_help()
 
