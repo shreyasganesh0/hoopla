5fc127bbdf81d4731d84d68be6ae7d6ba97dd771
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-10-16 09:30:06 -0400

    feat: add document embeddings creation/load functionality

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,11 +1,17 @@
 from sentence_transformers import SentenceTransformer
 import string
+import json
+import os
+import numpy as np
 
 class SemanticSearch:
 
     def __init__(self):
 
         self.model = SentenceTransformer('all-MiniLM-L6-v2') 
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
 
     def generate_embeddings(self, text):
 
@@ -15,6 +21,55 @@ class SemanticSearch:
         embedding = self.model.encode([text])
         
         return embedding[0]
+    
+    def build_embeddings(self, documents):
+
+        doc_list = []
+
+        for v in documents:
+
+            doc_list.append(f"{v["title"]}: {v["description"]}")
+
+        self.embeddings = self.model.encode(doc_list, show_progress_bar = True)
+        
+        with open("cache/movie_embeddings.py", "wb") as f_embed:
+
+            np.save(f_embed, self.embeddings)
+
+        return self.embeddings
+    
+    def load_or_create_embeddings(self, documents):
+
+        self.documents = documents
+
+        for doc in documents:
+
+            self.document_map[doc["id"]] = doc
+
+        if os.path.exists("cache/movie_embeddings.py"):
+
+            self.embeddings = np.load("cache/movie_embeddings.py")
+
+            if len(self.embeddings) != len(documents):
+
+                return self.embeddings
+        else:
+
+            return self.build_embeddings(documents)
+
+def verify_embeddings():
+
+    sem_search = SemanticSearch()
+
+    documents = []
+    with open("data/movies.json", "r") as f_movies:
+        data = json.load(f_movies)
+        documents = data["movies"]
+    embeddings = sem_search.load_or_create_embeddings(documents)
+    print(f"Number of docs:   {len(documents)}")
+    print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
+
+
 def embed_text(text):
 
     sem_search = SemanticSearch()
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-from lib.semantic_search import verify_model, embed_text
+from lib.semantic_search import verify_model, verify_embeddings, embed_text
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -10,6 +10,10 @@ def main():
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser("verify", help="Verify existance of model")
+
+    verify_embeddings_parser = subparsers.add_parser("verify_embeddings", 
+                                                     help="Verify embeddings of documents")
+
     embed_parser = subparsers.add_parser("embed_text", help="Generate embedding for text")
     embed_parser.add_argument("text", type=str, help="Word to find embedding of")
 
@@ -20,6 +24,10 @@ def main():
         case "verify":
 
             verify_model()
+
+        case "verify_embeddings":
+
+            verify_embeddings()
 
         case "embed_text":
 
